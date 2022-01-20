6cc991934130574da725af3702e7e2503b88232b
Tevis <hoyin0929@gmail.com>
2022-01-20 12:58:49 -0800

    created API for front and backend

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const dbService = require('./dbService');
 
 app.use(cors());
 app.use(express.json());
@@ -12,19 +13,56 @@ app.use(express.urlencoded({ extended : false }));
 
 //create
 app.post('/insert', (request, response) => {
+    console.log('I got a request!')
+    console.log(request.body);
+    const { name } = request.body;
+    const db = dbService.getDbServiceInstance();
 
+    const result = db.insertNewName(name);
+
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
 });
 
 //read
 app.get('/getAll', (request, response) => {
-    console.log('test');
-    response.redirect("../cilent/index.html");
-    //response.send('hello');
-})
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.getAllData();
+    
+    result
+    .then(data => response.json({data : data}))
+    .catch(err => console.log(err));
+});
 
 //update
+app.patch('/update', (request, response) => {
+    console.log(request.body);
+    const {id, tip_per} = request.body;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.updateTipById(id, tip_per);
+
+    result
+    .then(data => response.json({success : data}))
+    .catch(err => console.log(err));
+
+});
 
 
 //delete
+app.delete('/delete/:id', (request, response) => {
+    //console.log(request.params);
+    const {id} = request.params;
+    const db = dbService.getDbServiceInstance();
+
+    const result = db.deleteRowById(id);
+    
+    result
+    .then(data => response.json({success: data}))
+    .catch(err => console.log(err));
+
+});
 
 app.listen(process.env.PORT, () => console.log('app is tunning'));
---
+++ server/dbService.js
@@ -0,0 +1,106 @@
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+let instance = null;
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host:process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
+
+connection.connect((err) => {
+    if (err){
+        console.log(err.message);
+    }
+    console.log('db ' + connection.state);
+})
+
+class DbService{
+    static getDbServiceInstance(){
+            return instance ? instance : new DbService();
+    }
+
+    async getAllData(){
+        try{
+            const response = await new Promise((resolve, reject) => {
+                const query = "SELECT * FROM employee;";
+
+                connection.query(query, (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results);       
+                })
+            });
+            //console.log(response);
+            return response;
+        } catch (error){
+            console.log(error);
+        }
+    }
+
+    async insertNewName(name){
+        try{
+            const tip_per = 0.3;
+            const insertId = await new Promise((resolve, reject) => {
+                const query = "INSERT INTO employee (name, tip_per) VALUES(?,?);";
+
+                connection.query(query, [name, tip_per] , (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results.insertId);
+                })
+            });
+        //console.log(insertId);
+            return {
+                id : insertId,
+                name : name,
+                tip_per: tip_per
+         };
+            //return insertId;
+
+        }catch(error){
+            console.log(err);
+        }
+    }
+
+    async deleteRowById(id) {
+        try{
+            id = parseInt(id, 10);
+            const response = await new Promise((resolve, reject) => {
+                const query = "DELETE FROM employee WHERE id = ?";
+
+                connection.query(query, [id] , (err, results) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(results.affectedRows);
+                })
+            });
+            //console.log(response);
+            return response === 1? true : false;
+        }catch (error) {
+            console.log(error);
+        }
+
+    }
+
+    async updateTipById(id, tip_per){
+        try{
+            id = parseInt(id, 10);
+            const response = await new Promise((resolve, reject) => {
+                const query = "UPDATE employee SET tip_per = ? WHERE id = ?";
+
+                connection.query(query, [tip_per, id] , (err, result) => {
+                    if (err) reject(new Error(err.message));
+                    resolve(result.affectedRows);
+                })
+            });
+            //console.log(response.body);
+            return response === 1? true : false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    }
+}
+
+module.exports = DbService;
