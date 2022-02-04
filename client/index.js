9ffb33be27724b5bd500efbcfe7c951b8fd7c7dd
Tevis <hoyin0929@gmail.com>
2022-02-04 15:34:51 -0800

    added missing fetch

--- client/index.js
+++ client/index.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-    fetch('http://localhost:5000/getAll')
+    fetch('https://restaurant-calculate-system.herokuapp.com/getAll')
     .then(response => response.json())
     .then(data => loadHTMLTable(data['data']));
     
