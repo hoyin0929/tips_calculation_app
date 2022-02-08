29be84f0ebee0452418ca579ec6d83ddaafb5390
Tevis <hoyin0929@gmail.com>
2022-02-08 02:27:31 -0800

    date reformatted

--- client/script.js
+++ client/script.js
@@ -248,7 +248,7 @@ function loadTipTable(data){
     data.forEach(function({name, date, amTip, pmTip}){
         tableHtml += "<tr>";
         tableHtml += `<td class="name">${name}</td>`;
-        tableHtml += `<td>${new Date(date.replace(/-/g, '\/')).toLocoleDateString()}</td>`;
+        tableHtml += `<td>${new Date(date).replace(/-/g, '\/').toLocoleDateString()}</td>`;
         tableHtml += `<td>$${amTip}</td>`;
         tableHtml += `<td>$${pmTip}</td>`;
         tableHtml += "</tr>";
